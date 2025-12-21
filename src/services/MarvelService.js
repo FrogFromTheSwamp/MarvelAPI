7f0c9eaf6878bacb2c0e8d99b0a84994b73867b0
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-12-21 16:31:42 +0300

    Создан компонент со случайным персанажем

---
+++ src/services/MarvelService.js
@@ -0,0 +1,35 @@
+
+class MarvelService {
+    _apiBase = 'https://api.jikan.moe/v4/';
+
+    getResource= async (url) => {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
+        }
+        return await res.json();
+    }
+
+     getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters`);
+        console.log(res.data); 
+    }
+
+    getCharacter = async (id) => { 
+        const res = await this.getResource(`${this._apiBase}characters/${id}`);
+        return this._transformCharacter(res.data);
+    }
+
+    _transformCharacter = (char) => { 
+        return ({
+            name: char.name,
+            description: char.about,
+            thumbnail: char.images.jpg.image_url,
+            homepage: char.url,
+            wiki: char.url,
+        })
+    }
+}
+
+
+export default MarvelService;
